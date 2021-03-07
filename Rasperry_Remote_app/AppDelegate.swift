627446b8c95e8c265ca8381bcfb07de891bf3788
lw-classy <levinwalser@gmx.de>
2021-03-07 23:21:25 +0100

    remove Storyboard iOS is for hackers

--- Rasperry_Remote_app/AppDelegate.swift
+++ Rasperry_Remote_app/AppDelegate.swift
@@ -12,11 +12,20 @@ import CoreData
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
+    var window: UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        return true
+        if #available(iOS 13.0, *){
+            return true
+        } else {
+            self.window = UIWindow(frame: UIScreen.main.bounds)
+            self.window?.rootViewController = ViewController()
+            self.window?.makeKeyAndVisible()
+            
+            return true
+            
+        }
     }
 
     // MARK: UISceneSession Lifecycle
