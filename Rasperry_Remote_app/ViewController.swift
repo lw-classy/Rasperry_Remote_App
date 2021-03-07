1a8bf90e5806588fc0d7a3e26cd342ecd361fda1
lw-classy <levinwalser@gmx.de>
2021-03-07 21:48:39 +0100

    Initial Commit

---
+++ Rasperry_Remote_app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rasperry_Remote_app
+//
+//  Created by LevinJungstier on 07.03.21.
+//  Copyright © 2021 LevinJungstier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Rasperry_Remote_appTests/Rasperry_Remote_appTests.swift
@@ -0,0 +1,34 @@
+//
+//  Rasperry_Remote_appTests.swift
+//  Rasperry_Remote_appTests
+//
+//  Created by LevinJungstier on 07.03.21.
+//  Copyright © 2021 LevinJungstier. All rights reserved.
+//
+
+import XCTest
+@testable import Rasperry_Remote_app
+
+class Rasperry_Remote_appTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
